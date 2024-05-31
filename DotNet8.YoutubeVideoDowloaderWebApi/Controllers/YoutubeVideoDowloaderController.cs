045537f551d19e1303877fffb8b33f88502cae60
minthukha <minthukha1239788@gmail.com>
2024-05-31 13:44:24 +0630

    Add project files.

---
+++ DotNet8.YoutubeVideoDowloaderWebApi/Controllers/YoutubeVideoDowloaderController.cs
@@ -0,0 +1,84 @@
+﻿using DotNet8.YoutubeVideoDowloaderWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using YoutubeExplode;
+
+namespace DotNet8.YoutubeVideoDowloaderWebApi.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class YoutubeVideoDowloadercontroller : ControllerBase
+    {
+        [HttpPost("dowload")]
+        public async Task<IActionResult> DowloadVideos(DownloadRequestModel reqModel)
+        {
+            #region Check Requir Field
+
+            if (reqModel.OutputFilePath is null)
+            {
+                return BadRequest("No video outputDirectory provided.");
+            }
+
+            if (reqModel.VideoUrls is null)
+            {
+                return BadRequest("No video URLs provided.");
+            }
+
+            #endregion
+
+            #region Called DownloadYouTubeVideo Method To Dowload 
+
+            try
+            {
+                foreach (var videoUrl in reqModel.VideoUrls)
+                {
+                    await DownloadYouTubeVideo(videoUrl, reqModel.OutputFilePath);
+                }
+
+                if (reqModel.VideoUrls.Count is 1)
+                {
+                    return Ok("Download completed video.");
+                }
+                return Ok("Download completed for all videos.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred while downloading the videos: {ex.Message}");
+            }
+
+            #endregion
+
+        }
+
+        #region DownloadYouTubeVideo
+        private async Task DownloadYouTubeVideo(string videoUrl, string outputDirectory)
+        {
+            var youtube = new YoutubeClient();
+            var video = await youtube.Videos.GetAsync(videoUrl);
+
+            string sanitizedTitle = string.Join("_", video.Title.Split(Path.GetInvalidFileNameChars()));
+
+            var streamManifest = await youtube.Videos.Streams.GetManifestAsync(video.Id);
+            var muxedStreams = streamManifest.GetMuxedStreams().OrderByDescending(s => s.VideoQuality).ToList();
+
+            if (muxedStreams.Any())
+            {
+                var streamInfo = muxedStreams.First();
+                using var httpClient = new HttpClient();
+                var stream = await httpClient.GetStreamAsync(streamInfo.Url);
+                var datetime = DateTime.Now;
+
+                string outputFilePath = Path.Combine(outputDirectory, $"{sanitizedTitle}.{streamInfo.Container}");
+                using var outputStream = System.IO.File.Create(outputFilePath);
+                await stream.CopyToAsync(outputStream);
+            }
+            else
+            {
+                throw new Exception($"No suitable video stream found for {video.Title}.");
+            }
+
+        }
+
+        #endregion
+
+    }
+}
---
+++ DotNet8.YoutubeVideoDowloaderWebApi/Models/DownloadRequestModel.cs
@@ -0,0 +1,7 @@
+﻿namespace DotNet8.YoutubeVideoDowloaderWebApi.Models;
+
+public class DownloadRequestModel
+{
+    public string OutputFilePath { get; set; }
+    public List<string> VideoUrls { get; set; }
+}
